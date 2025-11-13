5c3ce2d02da6ffc7903cef90e13a76243fbcd39e
valeriavalencia766 <valeriavalencia766@gmail.com>
2025-11-13 20:04:24 +0000

    feature(LoginForm): LoginForm  inputs centered refixed to 50%

--- src/components/features/auth/LoginForm.tsx
+++ src/components/features/auth/LoginForm.tsx
@@ -58,13 +58,13 @@ export const LoginForm = () => {
       <VStack spacing={6} align="center">
 
         {error && (
-          <Alert status="error" width="60%">
+          <Alert status="error" width="50%">
             <AlertIcon />
             {error}
           </Alert>
         )}
 
-        <FormControl isInvalid={!!errors.email} width="60%">
+        <FormControl isInvalid={!!errors.email} width="50%">
           <FormLabel htmlFor="email">Email</FormLabel>
           <Input
             id="email"
@@ -80,7 +80,7 @@ export const LoginForm = () => {
           {errors.email && <Box color="red.500" mt={2}>{errors.email.message}</Box>}
         </FormControl>
 
-        <FormControl isInvalid={!!errors.password} width="60%">
+        <FormControl isInvalid={!!errors.password} width="50%">
           <FormLabel htmlFor="password">Contraseña</FormLabel>
           <Input
             id="password"
@@ -101,7 +101,7 @@ export const LoginForm = () => {
           colorScheme="blue"
           isLoading={isLoading}
           loadingText="Iniciando..."
-          width="60%"
+          width="50%"
         >
           Entrar
         </Button>
